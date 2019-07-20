12020029b58152d1d9006819f36259212fd246e8
Mark Xavier <mark.j.xavier@gmail.com>
2019-07-20 19:07:17 -0400

    Fleshed out main
    Now main accepts both verbosity and help optional arguments, and
    has two required arguments.  It also prints a description and
    usage instructions to the screen.
    
    Next step is to define an input file format, flesh out the README
    a bit more, and start file reading.  I'll also need to create a
    file writer object, and potentially other objects to store info
    on the filepath, title, etc.

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,58 @@
  */
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <unistd.h>
+
+void printUsage()
+{
+  std::cout << "\n##################################################################\n"
+            << "USAGE:\n\n"
+            << "This program generates a bare-bones HTML document based on an\n"
+            << "input file notating the table of contents structure.  The\n"
+            << "program must be called as follows:\n\n"
+            << "DSMC_HTML_Generator [-h] [-v] <toc_structure_file> <output_filepath>\n\n"
+            << "For information on the file structure of the two required arguments\n"
+            << "refer to the included README.\n\n"
+            << "-h : Help, prints this usage information.\n"
+            << "-v : Verbose, increased logging to console.\n"
+            << "##################################################################\n\n";
+}
 
 int main(int argc, char* argv[])
 {
-  std::cout << "Test Printing\n";
+  bool verbose = false;
+  int c;
+
+  if (argc < 2)
+  {
+    std::cout << "[*]Error: This program expect at least 2 arguments\n";
+    printUsage();
+    std::cout << "Program Exiting.\n";
+    return 1;
+  }
+
+  while((c = getopt(argc, argv, "hv")) != -1)
+  {
+    switch(c)
+    {
+      case 'h':
+        printUsage();
+        return 0;
+      case 'v':
+        verbose = true;
+        break;
+      case '?':
+        std::cerr << "Unknown argument " << optopt << " passed.\n";
+        return 1;
+      default:
+        std::abort();
+    }
+  }
+
+  std::string inputFileName(argv[optind++]);
+  std::string outputFileName(argv[optind]);
 
   return 0;
 }

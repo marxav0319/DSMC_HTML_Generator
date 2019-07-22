48ba699f48be851d2630cf71d9a588d97ab3ecfe
Mark <mark.j.xavier@gmail.com>
2019-07-22 17:21:20 -0400

    Working on cleanup, adding constants.h to do that

---
+++ src/constants.h
@@ -0,0 +1,24 @@
+/**
+ * This file is part of the DSMC_HTML_Generator package.
+ *
+ * This header file defines constants to be used across the source files included in this directory
+ * (src) and below.
+ *
+ * Author: Mark Xavier
+ */
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+
+// HTML tags to ignore
+const std::string DOC_START = "<!DOCTYPE";
+const std::string HTML_START = "<html>";
+const std::string HTML_END = "</html>";
+const std::string HEAD_START = "<head>";
+const std::string HEAD_END = "</head>";
+const std::string BODY_START = "<body>";
+const std::string BODY_END = "</body>";
+
+#endif // CONSTANTS_H
